915bed5cc22523ec4cb6b261e253cdcb0757bdc7
Tom Lai <tomtclai@gmail.com>
2015-06-22 21:00:49 -0700

    compiles

--- Instant Lyrics/Instant Lyrics/ViewController.h
+++ Instant Lyrics/Instant Lyrics/ViewController.h
@@ -11,8 +11,8 @@
 @interface ViewController : UIViewController
 -(BOOL)webView:(nonnull UIWebView *)webView shouldStartLoadWithRequest:(nonnull NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType;
 -(void)webViewDidFinishLoad:(nonnull UIWebView *)webView;
--(void)swipeBack:(__nonnull UIScreenEdgePanGestureRecognizer *)regcognizer;
--(void)swipeForward:(__nonnull UIScreenEdgePanGestureRecognizer *)regcognizer;
+-(void)swipeBack:(nonnull UIScreenEdgePanGestureRecognizer *)regcognizer;
+-(void)swipeForward:(nonnull UIScreenEdgePanGestureRecognizer *)regcognizer;
 typedef enum searchOptions
 {
     SEARCH_OPTIONAL,
