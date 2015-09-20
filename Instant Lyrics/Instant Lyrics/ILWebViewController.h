f8e0ffbba940467ab05ce268a7b712f5601d2864
Tom Lai <tomtclai@gmail.com>
2015-09-20 13:02:24 -0700

    no message

---
+++ Instant Lyrics/Instant Lyrics/ILWebViewController.h
@@ -0,0 +1,13 @@
+//
+//  ILWebViewController.h
+//  Instant Lyrics
+//
+//  Created by Tom Lai on 9/20/15.
+//  Copyright © 2015 Tom. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ILWebViewController : ViewController
+
+@end
